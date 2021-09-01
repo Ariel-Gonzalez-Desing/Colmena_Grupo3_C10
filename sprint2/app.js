4d63cf56db2c122d8fb291525007eac1fab54dd7
danydeitu <danydeitu18@gmail.com>
2021-09-01 11:05:20 -0300

    subo header

--- sprint2/app.js
+++ sprint2/app.js
@@ -6,12 +6,10 @@ const port = 3030;
 app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')));
-app.get('/carrito',(req,res) => res.sendFile(path.join(__dirname,'views','carrito.html')));
-app.get('/detalle',(req,res) => res.sendFile(path.join(__dirname,'views','detalle.html')));
-app.get('/footer',(req,res) => res.sendFile(path.join(__dirname,'views','footer.html')));
 app.get('/header',(req,res) => res.sendFile(path.join(__dirname,'views','header.html')));
 app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
 app.get('/registro',(req,res) => res.sendFile(path.join(__dirname,'views','registro.html')));
+
 
 
 
