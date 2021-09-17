936f1c8f20cc5fa517b17015e8b9a17b320d43e7
Ariel <arygonzalez2014@gmail.com>
2021-09-17 13:32:26 -0300

    agrego cambios index

--- Site/routes/products.js
+++ Site/routes/products.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {detalle, carrito, add, edit} = require('../controllers/productsController');
 
 /* /products */
-router.get('/detalle', detalle);
+router.get('/detalle/:id', detalle);
 router.get('/carrito', carrito);
 router.get('/add', add);
 router.get('/edit/:id', edit);
