34538349af29619f3babc8cf0a70a4420710af82
imnotinsane <s.joeljurado@hotmail.com>
2021-10-17 19:03:59 -0300

    cambios parciales de session

--- Site/src/app.js
+++ Site/src/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride =  require('method-override');
+const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(methodOverride('_method'));
+app.use(session({secret: "abeja secreta"}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
