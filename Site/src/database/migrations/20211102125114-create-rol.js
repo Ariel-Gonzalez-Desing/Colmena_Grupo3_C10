e74287a00b60433282838ef906f59643b95ec158
imnotinsane <s.joeljurado@hotmail.com>
2021-11-08 11:39:49 -0300

    errores sin arreglar, buscar solucion

--- Site/src/database/migrations/20211102125114-create-rol.js
+++ Site/src/database/migrations/20211102125114-create-rol.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('rols', {
+    await queryInterface.createTable('Rols', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('rols');
+    await queryInterface.dropTable('Rols');
   }
 };
