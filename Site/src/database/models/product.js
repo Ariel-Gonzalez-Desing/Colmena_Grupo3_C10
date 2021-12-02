aecbf5a9d466404549e2ece5a70fef909c3944f5
Ariel Gonzalez <arygonzalez2014@gmail.com>
2021-12-02 10:04:32 -0300

    agrego cambios

--- Site/src/database/models/product.js
+++ Site/src/database/models/product.js
@@ -9,26 +9,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-<<<<<<< HEAD
-     static associate(models) {
-=======
     static associate(models) {
->>>>>>> ea8c61df458ceff1e8132c0f5450430e3b813663
       // define association here
       Product.hasMany(models.Image,{
         as: 'images',
         onDelete: 'cascade',
-<<<<<<< HEAD
-        foreignKey: 'productId'
-      }),
-      Product.belongsTo(models.Category,{
-        as : 'category',
-        foreignKey : 'categoryId'
-      }),
-      Product.belongsTo(models.Display,{
-        as : 'display',
-        foreignKey : 'displayId'
-=======
         key: 'productId'
       }),
       Product.belongsTo(models.Category,{
@@ -38,7 +23,6 @@ module.exports = (sequelize, DataTypes) => {
       Product.belongsToMany(models.Cart,{
         as : 'cart',
         key : 'productId'
->>>>>>> ea8c61df458ceff1e8132c0f5450430e3b813663
       })
     }
   };
@@ -46,13 +30,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     description: DataTypes.STRING,
     price: DataTypes.DECIMAL,
-<<<<<<< HEAD
-    size: DataTypes.STRING,
-    categoryId: DataTypes.INTEGER,
-    displayId: DataTypes.INTEGER
-=======
     categoryId: DataTypes.INTEGER
->>>>>>> ea8c61df458ceff1e8132c0f5450430e3b813663
   }, {
     sequelize,
     modelName: 'Product',
